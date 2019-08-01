20d1e696c121317f44b933480454a24392e3f852
Brian Caball <tolcreator@yahoo.co.uk>
2019-08-02 00:15:05 +0100

    Changed uwp from old class and numbers based to new string based

--- uwp.py
+++ uwp.py
@@ -1,233 +1,263 @@
 import dice
 
-uwp = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+FIELDS = ['Starport', 'Size', 'Atmosphere', 'Hydrosphere', 'Population', 'Government', 'Law', 'Technology']
 
-def intToUwp(i):
-    return uwp[i]
+HEXES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+         'U', 'V', 'W', 'X', 'Y', 'Z']
 
-class Uwp:
-    def __init__(self):
-        self.starport = 'X'
-        self.size = 0
-        self.atmosphere = 0
-        self.hydrosphere = 0
-        self.population = 0
-        self.government = 0
-        self.lawlevel = 0
-        self.techlevel = 0
- 
-    def generate(self):
-        self.size = self.generateSize()
-        self.atmosphere = self.generateAtmosphere(self.size)
-        self.hydrosphere = self.generateHydrosphere(self.size, self.atmosphere)
-        self.population = self.generatePopulation(self.size, self.atmosphere)
-        self.starport = self.generateStarport(self.population)
-        self.government = self.generateGovernment(self.population)
-        self.lawlevel = self.generateLawLevel(self.population, self.government)
-        self.techlevel = self.generateTechLevel(self.starport, self.size, self.atmosphere, self.hydrosphere, self.population, self.government)
-    
-    def __str__(self):
-        return self.starport + intToUwp(self.size) + intToUwp(self.atmosphere) + intToUwp(self.hydrosphere) + intToUwp(self.population) + intToUwp(self.government) + intToUwp(self.lawlevel) + '-' + intToUwp(self.techlevel) +\
-           "    " + self.getTradeCodes()
+NUMBERS = {
+    '0': 0,  '1': 1,  '2': 2,  '3': 3,  '4': 4,  '5': 5,  '6': 6,  '7': 7,  '8': 8,  '9': 9,
+    'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'G': 16, 'H': 17, 'I': 18, 'J': 19,
+    'K': 20, 'L': 21, 'M': 22, 'N': 23, 'O': 24, 'P': 25, 'Q': 26, 'R': 27, 'S': 28, 'T': 29,
+    'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34, 'Z': 35
+}
 
-    def getTradeCodes(self):
-        codes = ""
-        if self.atmosphere in [4, 5, 6, 7, 8, 9] and self.hydrosphere in [4, 5, 6, 7, 8] and self.population in [5, 6, 7]:
-            codes = codes + "Ag "
-        if self.size == 0:
-            codes = codes + "As "
-        if self.population == 0:
-            codes = codes + "Ba "
-        if self.atmosphere >= 2 and self.hydrosphere == 0:
-            codes = codes + "De "
-        if self.atmosphere >= 10 and self.hydrosphere > 0:
-            codes = codes + "Fl "
-        if self.size >= 5 and self.atmosphere in [4, 5, 6, 7, 8, 9] and self.hydrosphere in [4, 5, 6, 7, 8]:
-            codes = codes + "Ga "
-        if self.population >= 9:
-            codes = codes + "Hi "
-        if self.techlevel >= 12:
-            codes = codes + "Ht "
-        if self.atmosphere in [0, 1] and self.hydrosphere > 0:
-            codes = codes + "Fl "
-        if self.atmosphere in [0, 1, 2, 4, 7, 9] and self.population >= 9:
-            codes = codes + "In "
-        if self.population <= 3:
-            codes = codes + "Lo "
-        if self.techlevel <= 5:
-            codes = codes + "Lt "
-        if self.atmosphere in [0, 1, 2, 3] and self.hydrosphere in [0, 1, 2, 3] and self.population >= 6:
-            codes = codes + "Na "
-        if self.population <= 6:
-            codes = codes + "Ni "
-        if self.atmosphere in [2, 3, 4, 5] and self.hydrosphere in [0, 1, 2, 3]:
-            codes = codes + "Po "
-        if self.atmosphere in [6, 8] and self.population in [6, 7, 8] and self.government in [4, 5, 6, 7, 8, 9]:
-            codes = codes + "Ri "
-        if self.atmosphere == 0:
-            codes = codes + "Va "
-        if self.hydrosphere > 9:
-            codes = codes + "Wa "
-        return codes
 
-    def generateSize(self):
-        return dice.roll(2, 6) - 2;
+def sanityCheck(suwp):
+    """
+    Check that string suwp is in the format:
+    X000000-0
+    :param suwp:
+    :return:
+    """
+    # Gotta be the right length
+    if len(suwp) != 8:
+        raise ValueError
 
-    def generateAtmosphere(self, size):
+    # Gotta have that dash
+    if suwp[7] not in ['-']:
+        raise ValueError
+
+    starport = suwp[0]
+    size = suwp[1]
+    atmosphere = suwp[2]
+    hydrosphere = suwp[3]
+    population = suwp[4]
+    government = suwp[5]
+    law = suwp[6]
+    tech = suwp[8]
+
+    if starport not in ['A', 'B', 'C', 'D', 'E', 'X']:
+        raise ValueError
+    if size not in HEXES[1:10]:
+        raise ValueError
+    if atmosphere not in HEXES[1:15]:
+        raise ValueError
+    if hydrosphere not in HEXES[1:10]:
+        raise ValueError
+    if population not in HEXES[1:10]:
+        raise ValueError
+    # The rest of these can be arbitrarily high
+    if government not in HEXES:
+        raise ValueError
+    if law not in HEXES:
+        raise ValueError
+    if tech not in HEXES:
+        raise ValueError
+
+
+def strToUwp(suwp):
+    """
+    :param suwp: A string with proper UWP format
+    :return: A dictionary of the UWP values with keys from FIELDS
+    """
+    try:
+        sanityCheck(suwp)
+    except ValueError:
+        return None
+    suwp = suwp.replace('-', '')
+    uwp = {}
+    for i in range(len(suwp)):
+        uwp[FIELDS[i]] = suwp[i]
+    return uwp
+
+def uwpToStr(uwp):
+    ret = ""
+    ret += uwp['Starport']
+    ret += uwp['Size']
+    ret += uwp['Atmosphere']
+    ret += uwp['Hydrosphere']
+    ret += uwp['Population']
+    ret += uwp['Government']
+    ret += uwp['Law']
+    ret += '-'
+    ret += uwp['Technology']
+    return ret
+
+def generate():
+    """
+    Generates a new UWP
+    :return: A dictionary of the new UWP with keys from FIELDS
+    """
+    uwp = {}
+    uwp['Size'] = generateSize()
+    uwp['Atmosphere'] = generateAtmosphere(uwp)
+    uwp['Hydrosphere'] = generateHydrosphere(uwp)
+    uwp['Population'] = generatePopulation(uwp)
+    uwp['Government'] = generateGovernment(uwp)
+    uwp['Law'] = generateLaw(uwp)
+    uwp['Starport'] = generateStarport(uwp)
+    uwp['Technology'] = generateTechnology(uwp)
+    return uwp
+
+
+def generateSize():
+    roll = dice.roll(2, 6) - 2
+    return HEXES[roll]
+
+
+def generateAtmosphere(uwp, spaceOpera=True):
+    size = NUMBERS[uwp['Size']]
+    roll = dice.roll(2, 6) + size - 7
+    if roll < 0:
+        roll = 0
+
+    if spaceOpera:
         if size in [0, 1, 2]:
-            return 0
-        atmo = dice.roll(2, 6) + size - 7;
-        if atmo < 0:
-            return 0
-        if size in [3, 4]:  
-            if atmo in [0, 1, 2]:
-                return 0
-            if atmo in [3, 4, 5]:
-                return 1
-            return 10
-        return atmo
+            return '0'
+        if size in [3, 4]:
+            if roll in [0, 1, 2]:
+                return '0'
+            elif roll in [3, 4, 5]:
+                return '1'
+            else:
+                return 'A'
 
-    def generateHydrosphere(self, size, atmo):
-        if size in [0, 1]:
-            return 0
-        dm = 0
-        if size in [3, 4] and atmo == 10:
-            dm = -6
-        if atmo in [0, 1]:
-            dm = -6
-        elif atmo in [2, 3, 11, 12]:
-            dm = -4
-        hydro = dice.roll(2, 6) + size - 7 + dm
-        if hydro < 0:
-            return 0
-        if hydro > 10:
-            return 10
-        return hydro
+    return HEXES[roll]
 
-    def generatePopulation(self, size, atmo):
-        dm = 0
-        if size in [0, 1, 2] or size > 10:
-            dm = dm - 1
-        if atmo in [5, 6, 8]:
-            dm = dm + 1
+
+def generateHydrosphere(uwp, spaceOpera=True):
+    size = NUMBERS[uwp['Size']]
+
+    if size in [0, 1]:
+        return '0'
+
+    atmosphere = NUMBERS[uwp['Atmosphere']]
+    roll = dice.roll(2, 6) + size - 7
+
+    dm = 0
+    if atmosphere in [0, 1, 10, 11, 12]:
+        dm = -4
+
+    if spaceOpera:
+        if size in [3,4] and atmosphere in [10]:
+            dm += -6
+        if atmosphere in [0, 1]:
+            dm += -6
+        if atmosphere in [2, 3, 11, 12]:
+            dm += -4
+
+    roll += dm
+    if roll < 0:
+        roll = 0
+    return HEXES[roll]
+
+
+def generatePopulation(uwp, hard=True):
+    size = NUMBERS[uwp['Size']]
+    atmosphere = NUMBERS[uwp['Atmosphere']]
+
+    roll = dice.roll(2, 6) - 2
+    dm = 0
+
+    if hard:
+        if size in [0, 1, 2, 10]:
+            dm += -1
+        if atmosphere in [5, 6, 8]:
+            dm += 1
         else:
-            dm = dm - 1
-        pop = dice.roll(2, 6) - 2 + dm
-        if pop < 0:
-            return 0
-        return pop
-        
-    def generateGovernment(self, pop):
-        if pop == 0:
-            return 0
-        gov = dice.roll(2, 6) - 7 + pop
-        if gov < 0:
-            return 0
-        return gov
+            dm += -1
 
-    def generateLawLevel(self, pop, gov):
-        if pop == 0:
-            return 0
-        law = dice.roll(2, 6) - 7 + gov
-        if law < 0:
-            return 0
-        return law
+    roll += dm
+    if roll < 0:
+        roll = 0
+    return HEXES[roll]
 
-    def generateStarport(self, pop):
-        if pop == 0:
-            return 'X'
-        if pop in [1, 2]:
-            dm = -2
-        if pop in [3, 4]:
-            dm = -1
-        if pop in [5, 6, 7]:
-            dm = 0
-        if pop in [8, 9]:
+
+def generateGovernment(uwp):
+    population = NUMBERS[uwp['Population']]
+
+    if population == 0:
+        return '0'
+
+    roll = dice.roll(2, 6) + population - 7
+    if roll < 0:
+        roll = 0
+    return HEXES[roll]
+
+
+def generateLaw(uwp):
+    population = NUMBERS[uwp['Population']]
+    government = NUMBERS[uwp['Government']]
+
+    if population == 0:
+        return '0'
+
+    roll = dice.roll(2, 6) + government - 7
+    if roll < 0:
+        roll = 0
+    return HEXES[roll]
+
+
+def generateStarport(uwp, hard=False, mgt2e=True):
+    population = NUMBERS[uwp['Population']]
+
+    if population == 0:
+        return 'X'
+
+    roll = dice.roll(2, 6)
+    dm = 0
+
+    if hard:
+        dm = population - 7
+    if mgt2e:
+        if population in [8, 9]:
             dm = 1
-        if pop > 9:
+        elif population > 9:
             dm = 2
+        elif population in [3, 4]:
+            dm = -1
+        elif population in [1, 2]:
+            dm = -2
 
-        port = dice.roll(2, 6) + dm
-        if port < 2:
-            return 'X'
-        if port < 5:
-            return 'E'
-        if port < 7:
-            return 'D'
-        if port < 9:
-            return 'C'
-        if port < 11:
-            return 'B'
+    roll += dm
+    if roll > 10:
         return 'A'
-
-    def generateTechLevel(self, port, size, atmo, hydro, pop, gov):
-        if pop == 0:
-            return 0        
-        dm = 0
-        if port == 'X':
-            dm = dm - 4
-        elif port == 'C':
-            dm = dm + 2
-        elif port == 'B':
-            dm = dm + 4
-        elif port == 'A':
-            dm = dm + 6
-
-        if size < 2:
-            dm = dm + 2
-        elif size < 5:
-            dm = dm + 1
-
-        if atmo < 4:
-            dm = dm + 1
-        elif atmo > 9:
-            dm = dm + 1
-
-        if hydro in [0, 9]:
-            dm = dm + 1
-        elif hydro == 10:
-            dm = dm + 2
-
-        if pop < 6:
-            dm = dm + 1
-        elif pop > 8:
-            dm = dm + 1
-        if pop > 9:
-            dm = dm + 1
-        if pop > 10:
-            dm = dm + 1
-        if pop > 11:
-            dm = dm + 1
-
-        if gov in [0, 5]:
-            dm = dm + 1
-        elif gov == 7:
-            dm = dm + 2
-        elif gov in [13, 14]:
-            dm = dm - 2
-
-        tech = dice.roll(1, 6) + dm
-        if tech < 0:
-            return 0
-        return tech
+    if roll > 8:
+        return 'B'
+    if roll > 6:
+        return 'C'
+    if roll > 4:
+        return 'D'
+    if roll > 2:
+        return 'E'
+    return 'X'
 
 
+def generateTechnology(uwp, max=12):
+    if uwp['Population'] in ['0']:
+        return '0'
+    dm = 0
+    dm += {'A': 6, 'B': 4, 'C': 2}.get(uwp['Starport'], 0)
+    dm += {'0': 2, '1': 2, '2': 1, '3': 1, '4': 1}.get(uwp['Size'], 0)
+    if uwp['Atmosphere'] in ['0', '1', '2', '3', 'A', 'B', 'C', 'D', 'E', 'F']:
+        dm += 1
+    dm += {'A': 2, '9': 1, '0': 1}.get(uwp['Hydrosphere'], 0)
+    if uwp['Population'] in ['1', '2', '3', '4', '5', '9']:
+        dm += 1
+    if uwp['Population'] in ['A']:
+        dm += 2
+    dm += {'0': 1, '5': 1, '7': 2, 'D': -2, 'E': -2}.get(uwp['Government'], 0)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    roll = dice.roll(1, 6)
+    roll += dm
+    if roll < 0:
+        roll = 0
+    if roll > max:
+        if dm+1 > max:
+            roll = dm+1
+        else:
+            roll = max
+    return HEXES[roll]
